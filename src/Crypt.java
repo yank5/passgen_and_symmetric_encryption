f3ba9329f78448ab80c26cc139b1f4ca28edc780
yank5 <yank5@nycstudents.net>
2022-12-11 21:19:39 -0500

    works but needs fixing

--- src/Crypt.java
+++ src/Crypt.java
@@ -1,5 +1,3 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-
 public class Crypt {
     private String text="";
     private String preGen="";
@@ -23,6 +21,9 @@ public class Crypt {
 
         String binString="";
 
+
+
+
         for (int stringArray=0;stringArray<g.length;++stringArray) { //foreach string in the string array
             var dd=g[stringArray].toCharArray();
             for(int x=0;x<g[stringArray].length();++x){ //for each character in the string
@@ -32,6 +33,8 @@ public class Crypt {
 //            System.out.print(" ");
             binString+=" ";
         }
+
+
 
 //        System.out.println("binstring:\n"+binString);
 
@@ -56,7 +59,7 @@ public class Crypt {
             binString+=l;
         }
 
-        System.out.println("padded binstring:\n"+binString);
+//        System.out.println("padded binstring:\n"+binString);
 
         preGen=binString;
 
@@ -67,36 +70,78 @@ public class Crypt {
 //        System.out.println("DEBUG BINSTRING "+binString);
         var y=key.toCharArray();
         var teststring="";
-        System.out.println("text encrypted:");
+        System.out.println("\ntext encrypted:");
         for(int p=0;p<x.length;++p){ //XOR ENCRYPT WORKING LETS GOOOOOOOOO
             teststring+=(x[p]^y[p]);
         }
-        System.out.print(teststring+"\nencrypted text readable form:\n"); //encrypted text
+        System.out.print(teststring); //encrypted text
 
-        var testStr=teststring.split("(?<=\\G.{" + 8 + "})");//splits binary string into 8 blocks.
-        for (int u = 0; u < testStr.length; u++) {
-            System.out.print((char) Long.parseLong(testStr[u], 2));//for each item in the array it decodes it into chars
-        }
+// FIXME: 12/11/2022
+//        var testStr=teststring.split("(?<=\\G.{" + 8 + "})");//splits binary string into 8 blocks.
+//        for (int u = 0; u < testStr.length; u++) {
+//            System.out.print((char) Long.parseLong(testStr[u], 2));//for each item in the array it decodes it into chars
+//        }
     }
 
 
 
 
     public void decrypt() {
-        var qq = text.toCharArray();
-        var y = key.toCharArray();
+
+        var qq = text.toCharArray(); //turns the inputted text to an array
+        var y = key.toCharArray(); //turns the key into an array
         StringBuilder decryp = new StringBuilder();
-        for (int p = 0; p < text.length(); ++p) {// TEXT DECRYPT
+
+
+/*
+FIXME: 12/11/2022
+        var binString="";
+        var g=text.split("");
+        for (int stringArray=0;stringArray<g.length;++stringArray) { //foreach string in the string array
+            var dd=g[stringArray].toCharArray();
+            for(int x=0;x<g[stringArray].length();++x){ //for each character in the string
+                binString+=(Integer.toBinaryString(dd[x]));
+                System.out.print(Integer.toBinaryString(dd[x]));
+            }
+//            System.out.print(" ");
+            binString+=" ";
+        }
+        String[] tee=binString.split(" "); //USE TEE AS NEW STRING ARRAY
+        for(int r=0;r<tee.length;r++){ //make sure they are all the same length with padding leading zeros
+            String temp="";
+            if(tee[r].length()<6){
+                temp=tee[r];
+                tee[r]="000"+temp;
+            } else if (tee[r].length()<7) {
+                temp=tee[r];
+                tee[r]="00"+temp;
+            } else if (tee[r].length()<8){
+                temp=tee[r];
+                tee[r]="0"+temp;
+            }
+        }
+        for (String l:tee) {
+            binString+=l; //makes binstring have correct padding
+        }
+        var x=binString.toCharArray(); //turns to chararray
+*/
+
+
+        for (int p = 0; p < text.length(); ++p) {// assumes that inputted key and text is binary already
 //            System.out.print(qq[p] ^ y[p]);
-            decryp.append(qq[p] ^ y[p]);
+            decryp.append(qq[p] ^ y[p]); //text decrypt
         }
         var decrypS = decryp.toString();
         String[] finalarray = decrypS.split("(?<=\\G.{" + 8 + "})");
-        System.out.println("\ntext decrypted in char form");
+        System.out.println("text decrypted in char form:");
+
+
+
         for (int u = 0; u < finalarray.length; u++) {
             System.out.print((char) Long.parseLong(finalarray[u], 2));
         }
     }
+
     private void keyGen(){//only call this method for encryption or else it will overwrite inputted key for decryption.
         System.out.println("KEY: ");
         for(long x=0L;x<preGen.length();x++){
@@ -104,13 +149,12 @@ public class Crypt {
             System.out.print(g);
             key+=g;
         }
-
-
-        System.out.println("\nkey in readable form");
-        var testKey=key.split("(?<=\\G.{" + 8 + "})");//splits binary string into 8 blocks.
-        for (int u = 0; u < testKey.length; u++) {
-            System.out.print((char) Long.parseLong(testKey[u], 2));//for each item in the array it decodes it into chars
-        }
-        System.out.println();
+// FIXME: 12/11/2022
+//        System.out.println("\nkey in readable form");
+//        var testKey=key.split("(?<=\\G.{" + 8 + "})");//splits binary string into 8 blocks.
+//        for (int u = 0; u < testKey.length; u++) {
+//            System.out.print((char) Long.parseLong(testKey[u], 2));//for each item in the array it decodes it into chars
+//        }
+//        System.out.println();
     }
 }
